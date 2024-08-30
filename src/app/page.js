7b8cc23c2bb11cfa6e9a3b338b834c58a8dcf868
Danial Sadiq Masood <danialsadiqmasood44@gmail.com>
2024-08-30 23:49:30 +0500

    add sidebar

--- src/app/page.js
+++ src/app/page.js
@@ -6,11 +6,11 @@ import CardDemo from '../shadComponents/sidebar'
 
 const HomePage = () => {
   return (
-    <div className='flex'>
-      <div className="relative min-h-screen w-[85vw] rounded-xl overflow-hidden">
+    <div className='flex h-[100vh] p-[20px] gap-[20px]'>
+      <div className="relative h-full pt-[20px] w-[85vw] rounded-xl overflow-hidden">
         <LocationAggregatorMap extensionKey='Final Votes' />
       </div>
-      <div className='p-5 flex items-center'>
+      <div className='flex'>
         <CardDemo />
       </div>
     </div>
--- src/shadComponents/sidebar.js
+++ src/shadComponents/sidebar.js
@@ -12,34 +12,20 @@ import {
     CardHeader,
     CardTitle,
 } from "@/components/ui/card"
+import { Label } from "@/components/ui/label"
+import { Switch } from "@/components/ui/switch"
 import Image from "next/image"
-
-const notifications = [
-    {
-        title: "Your call has been confirmed.",
-        description: "1 hour ago",
-    },
-    {
-        title: "You have a new message!",
-        description: "1 hour ago",
-    },
-    {
-        title: "Your subscription is expiring soon!",
-        description: "2 hours ago",
-    },
-]
-
 
 export default function CardDemo({ className, ...props }) {
     return (
-        <Card className={cn("w-full pt-6 min-w-[300px]", className)} {...props}>
-            <CardContent className="grid gap-2">
-                <div className=" flex flex-col items-center space-x-4 rounded-md border p-4">
+        <Card className={cn("w-full p-2 min-w-[300px]", className)} {...props}>
+            <CardContent className="p-2 grid gap-2">
+                <div className=" flex flex-col items-start space-x-4 rounded-md border p-3">
                     <div className="flex-1 space-y-1">
                         <p className="text-sm font-medium leading-none">
                             PML-N
                         </p>
-                        <p className="text-2xl font-bold text-muted-foreground">
+                        <p className="text-xl font-bold text-muted-foreground">
                             450
                         </p>
                         <p className="text-xs italic text-muted-foreground">
@@ -47,12 +33,12 @@ export default function CardDemo({ className, ...props }) {
                         </p>
                     </div>
                 </div>
-                <div className=" flex flex-col items-center space-x-4 rounded-md border p-4">
+                <div className=" flex flex-col items-start space-x-4 rounded-md border p-3">
                     <div className="flex-1 space-y-1">
                         <p className="text-sm font-medium leading-none">
                             IPP
                         </p>
-                        <p className="text-2xl font-bold text-muted-foreground">
+                        <p className="text-xl font-bold text-muted-foreground">
                             425
                         </p>
                         <p className="text-xs italic text-muted-foreground">
@@ -60,12 +46,12 @@ export default function CardDemo({ className, ...props }) {
                         </p>
                     </div>
                 </div>
-                <div className=" flex flex-col items-center space-x-4 rounded-md border p-4">
+                <div className=" flex flex-col items-start space-x-4 rounded-md border p-3">
                     <div className="flex-1 space-y-1">
                         <p className="text-sm font-medium leading-none">
                             PTI
                         </p>
-                        <p className="text-2xl font-bold text-muted-foreground">
+                        <p className="text-xl font-bold text-muted-foreground">
                             259
                         </p>
                         <p className="text-xs italic text-muted-foreground">
@@ -73,32 +59,44 @@ export default function CardDemo({ className, ...props }) {
                         </p>
                     </div>
                 </div>
+                <div className=" flex flex-col items-center space-x-4 rounded-md border p-4">
+                    <div className="flex gap-3 flex-col">
+                        <div className="flex items-start space-x-2">
+                            <Switch id="toggle-pti" />
+                            <Label htmlFor="airplane-mode">PTI Polling Station Results</Label>
+                        </div>
+                        <div className="flex items-start space-x-2">
+                            <Switch id="toggle-pti" />
+                            <Label htmlFor="airplane-mode">Non-PTI Polling Station Results</Label>
+                        </div>
+                    </div>
+                </div>
                 <div className="flex flex-col gap-0 items-stretch rounded-md border">
                     <div className="flex items-center space-x-4 border-b">
                         <div className="shrink-0">
-                            <Image src="/deck-map/nonptitile.png" width={50} height={50} />
+                            <Image src="/deck-map/nonptitile.png" width={40} height={40} />
                         </div>
                         <p className="text-xs italic">
                             Seat won by PML-N or IPP
                         </p>
                     </div>
                     <div className=" flex items-center space-x-4 border-b">
-                        <div className="shrink-0"> 
-                            <Image src="/deck-map/ptitile.png" width={50} height={50} />
+                        <div className="shrink-0">
+                            <Image src="/deck-map/ptitile.png" width={40} height={40} />
                         </div>
                         <p className="text-xs italic">
                             Seat won by PTI
                         </p>
                     </div>
                     <div className=" flex items-center gap-2 border-b">
-                        <div className="shrink-0 h-[50px] w-[50px] bg-[#5270d1]">
+                        <div className="shrink-0 h-[40px] w-[40px] bg-[#5270d1]">
                         </div>
                         <p className="text-xs italic">
                             Polling Station won by IPP or PML-N
                         </p>
                     </div>
                     <div className=" flex items-center gap-2">
-                        <div className="shrink-0 h-[50px] w-[50px] bg-[#d76e48]">
+                        <div className="shrink-0 h-[40px] w-[40px] bg-[#d76e48]">
                         </div>
                         <p className="text-xs italic">
                             Polling Station won by PTI
