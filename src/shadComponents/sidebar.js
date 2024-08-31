e49c921f6aa4b0f24b4815c9ed85ee597f0c9570
Danial Sadiq Masood <danialsadiqmasood44@gmail.com>
2024-08-31 12:29:26 +0500

    add left border to card

--- src/shadComponents/sidebar.js
+++ src/shadComponents/sidebar.js
@@ -16,59 +16,52 @@ import { Label } from "@/components/ui/label"
 import { Switch } from "@/components/ui/switch"
 import Image from "next/image"
 
+const turnOutData = [
+    {
+        party: 'PML-N',
+        turnout: 450
+    },
+    {
+        party: 'IPP',
+        turnout: 425
+    },
+    {
+        party: 'PTI',
+        turnout: 259
+    }
+]
+
 export default function CardDemo({ className, ...props }) {
     return (
         <Card className={cn("w-full p-2 min-w-[300px] h-fit", className)} {...props}>
             <CardContent className="p-2 grid gap-2">
-                <div className=" flex flex-col items-start space-x-4 rounded-md border p-3">
-                    <div className="flex-1">
-                        <p className="text-sm font-medium leading-none">
-                            PML-N
-                        </p>
-                        <p className="text-xl font-bold text-muted-foreground">
-                            450
-                        </p>
-                        <p className="text-xs italic text-muted-foreground">
-                            Average votes per Polling Station
-                        </p>
-                    </div>
-                </div>
-                <div className=" flex flex-col items-start space-x-4 rounded-md border p-3">
-                    <div className="flex-1">
-                        <p className="text-sm font-medium leading-none">
-                            IPP
-                        </p>
-                        <p className="text-xl font-bold text-muted-foreground">
-                            425
-                        </p>
-                        <p className="text-xs italic text-muted-foreground">
-                            Average votes per Polling Station
-                        </p>
-                    </div>
-                </div>
-                <div className=" flex flex-col items-start space-x-4 rounded-md border p-3">
-                    <div className="flex-1">
-                        <p className="text-sm font-medium leading-none">
-                            PTI
-                        </p>
-                        <p className="text-xl font-bold text-muted-foreground">
-                            259
-                        </p>
-                        <p className="text-xs italic text-muted-foreground">
-                            Average votes per Polling Station
-                        </p>
-                    </div>
-                </div>
+                {
+                    turnOutData.map(d => (
+                        <div key={d.party} className=" flex flex-col items-start space-x-4 rounded-md border p-3" style={{borderLeftWidth : (d.turnout/450) * 15}}>
+                            <div className="flex-1">
+                                <p className="text-sm font-medium leading-none">
+                                    {d.party}
+                                </p>
+                                <p className="text-xl font-bold text-muted-foreground">
+                                    {d.turnout}
+                                </p>
+                                <p className="text-xs italic text-muted-foreground">
+                                    Average votes per Polling Station
+                                </p>
+                            </div>
+                        </div>
+                    ))
+                }
                 <div className=" flex flex-col items-center space-x-4 rounded-md border p-4">
                     <div className="flex gap-3 flex-col">
                         {
-                        props.toggles
-                            .map((d,i) => (
-                                <div key={i} className="flex items-start space-x-2">
-                                    <Switch onCheckedChange={(checked) => d[1](checked)} checked={d[0]} id="toggle-pti" />
-                                    <Label htmlFor="airplane-mode">{d[2]}</Label>
-                                </div>
-                            ))
+                            props.toggles
+                                .map((d, i) => (
+                                    <div key={i} className="flex items-start space-x-2">
+                                        <Switch onCheckedChange={(checked) => d[1](checked)} checked={d[0]} id="toggle-pti" />
+                                        <Label htmlFor="airplane-mode">{d[2]}</Label>
+                                    </div>
+                                ))
                         }
                     </div>
                 </div>
