a3f9691b4dcff145d3708c58f1591c87e4a1bc3b
Danial Sadiq Masood <danialsadiqmasood44@gmail.com>
2024-09-05 13:54:59 +0500

    add bar charts

--- src/app/table/page.js
+++ src/app/table/page.js
@@ -13,13 +13,19 @@ import {
     CardTitle,
 } from "@/components/ui/card"
 
+import PollingStationChart from '@/shadComponents/pollingStationChart'
+
 const HomePage = () => {
 
     return (
         <div className="w-100 p-8">
             <Card>
-                <CardHeader className="pb-3">
+                <CardHeader className="pb-3 gap-3">
                     <CardTitle className="text-2xl">Polling Station Votes Breakdown</CardTitle>
+                    <div className='flex gap-4'>
+                        <PollingStationChart chartKey="pti" title="Polling Stations Results According to PTI"/>
+                        <PollingStationChart chartKey="est" title="Polling Stations Results According to ECP"/>
+                    </div>
                 </CardHeader>
                 <CardContent>
                     <DataTable />
